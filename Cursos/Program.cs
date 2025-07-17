53ae7e0840c691e69e6820dee0349ecec071b05d
lailaz <lailazappiello90@gmail.com>
2025-07-17 18:40:06 -0300

    feat: Projeto compelto!

--- Cursos/Program.cs
+++ Cursos/Program.cs
@@ -1,10 +1,10 @@
 ﻿using ProjetoEntendendoC.Models;
 
-Pessoa p1 = new Pessoa("Laila", "Zappiello", 18);
-Pessoa p2 = new Pessoa("Maria", "Silva", 22);
+Pessoa p1 = new Pessoa(nome: "Laila", sobrenome: "Zappiello", idade: 18);
+Pessoa p2 = new Pessoa(nome: "Luana", sobrenome: "Costa", idade: 36);
 
 
-Cursos cursoDeC = new Cursos("Curso de C#");
+Cursos cursoDeC = new Cursos(nome: "Curso de C#");
 cursoDeC.Alunos = new List<Pessoa>();
 cursoDeC.AdicionarAluno(p1);
 cursoDeC.AdicionarAluno(p2);
